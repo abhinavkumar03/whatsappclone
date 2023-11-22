62ee2056254ec02f9182fbb2eef41036f1a8e2d3
abhinavkumar03 <ak2711474@gmail.com>
2023-11-22 18:43:54 +0530

    display name and about

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,9 +1,13 @@
+import Input from "@/components/common/Input";
 import { useStateProvider } from "@/context/StateContext";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 function onboarding() {
   const [{userInfo}] = useStateProvider();
+  const [name, setName] = useState(userInfo?.name||"");
+  const [about, setAbout] = useState("");
+  const [image, setImage] = useState("/default_avatar.png");
   return ( <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center ">
     <div className="flex items-center justify-center gap-2">
       <Image src="/whatsapp.gif" alt="whatsapp" height={300} width={300}/>
@@ -12,7 +16,17 @@ function onboarding() {
     <h2 className="text-2xl">Create Your Profile</h2>
     <div className="flex gap-6 mt-6">
       <div className="flex flex-col items-center justify-center mt-5 gap-6">
-        {userInfo.name}
+        <Input 
+          name="Dispaly Name" 
+          state={name} 
+          setState={setName} 
+        label />
+        <Input 
+          name="About" 
+          state={about} 
+          setState={setAbout} 
+        label/>
+        
       </div>
     </div>
   </div>
