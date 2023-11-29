b5daa23d3f4807006176c0b2c0c7639a95117e45
abhinavkumar03 <ak2711474@gmail.com>
2023-11-29 16:34:15 +0530

    schema update

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -9,7 +9,7 @@ const getBaseUrl = () => {
     }
   };
 
-export const HOST = "https://whatsappclone-bgxe.onrender.com";
+export const HOST = "http://localhost:3005";
 
 const AUTH_ROUTE = `${HOST}/api/auth`;
 const MESSAGES_ROUTE = `${HOST}/api/messages`;
